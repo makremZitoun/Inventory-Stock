bd7298754e631660d2f5d310e8192255b9c35931
makremZitoun <makremzitoun242@gmail.com>
2023-12-25 22:46:05 +0100

    add Tf Output

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
-variable "region" {
-  type    = string
-  default = "West Europe"
-}
+# variable "region" {
+#   type    = string
+#   default = "West Europe"
+# }
 
-variable "vms" {
-  type    = string
-  default = "vm0"
-}
+# variable "vms" {
+#   type    = string
+#   default = "vm0"
+# }
 
-variable "vm_size" {
-  type    = string
-  default = "Standard_DS1_v2"
-}
+# variable "vm_size" {
+#   type    = string
+#   default = "Standard_DS1_v2"
+# }
